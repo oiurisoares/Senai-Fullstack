d93afea2dac2408e087eaa22a593d303683a4b52
Iuri Soares <108412671+oiurisoares@users.noreply.github.com>
2023-11-29 16:02:09 -0300

    Front-End-07b-SP2

---
+++ Front-End-07b-SP2/jquery-script.js
@@ -0,0 +1,11 @@
+//hide register form
+$(document).ready(function () {
+
+    $('#botao-cadastrar').click(function () {
+
+        $("#form-cadastrar").slideToggle("slow");
+        $("#section-login").slideToggle("slow");
+        $("#botao-cadastrar").hide();
+    });
+
+});
